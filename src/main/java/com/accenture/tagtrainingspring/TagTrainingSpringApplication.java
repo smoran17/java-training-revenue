05cf2a53a013b7688ab2e1cd6c6f5e67595f5968
Alan O Brien <alan.o.brien@accenture.com>
2021-05-25 10:36:14 +0100

    commit 2

--- src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
+++ src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
@@ -3,23 +3,23 @@ package com.accenture.tagtrainingspring;
 import com.accenture.tagtrainingspring.patient.Gender;
 import com.accenture.tagtrainingspring.patient.Patient;
 import com.accenture.tagtrainingspring.screening.Screening;
+import com.accenture.tagtrainingspring.screening.ScreeningDatabase;
 import com.accenture.tagtrainingspring.screening.ScreeningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class TagTrainingSpringApplication {
 
-	@Autowired
-	static ScreeningService screeningService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(TagTrainingSpringApplication.class, args);
 		printWelcomeMessage();
-		printlogic();
+		printlist();
 	}
 
 	private static void printWelcomeMessage() {
@@ -35,12 +35,46 @@ public class TagTrainingSpringApplication {
 
 	}
 
-	private static void printlogic(){
-		Patient patient = new Patient("John", 1234, LocalDate.of(1960, 01, 01), Gender.MALE);
-		Screening screening = new Screening(1000,patient,LocalDate.of(2021,01,01),false);
-		System.out.println("Patient: " + patient.getName() + " has a malignant diagnosis of " + screening.isMalignant());
+//	private static void printlogic(){
+//		Patient patient = new Patient("John", 1234, LocalDate.of(1960, 01, 01), Gender.MALE);
+//		Screening screening = new Screening(1000,patient,LocalDate.of(2021,01,01),false);
+//		System.out.println("Patient: " + patient.getName() + " has a malignant diagnosis of " + screening.isMalignant());
 //		ScreeningService screeningService = new ScreeningService();
-		System.out.println(screeningService.isPatientScreening(screening,patient));
+//		System.out.println(screeningService.isPatientScreening(screening,patient));
+//	}
+
+	private static void printlist(){
+		Patient patient = new Patient("John", 1234, LocalDate.of(1960, 01, 01), Gender.MALE);
+		Patient patient2 = new Patient("Mary", 1234, LocalDate.of(1960, 01, 01), Gender.FEMALE);
+		Patient patient3 = new Patient("Luke", 1234, LocalDate.of(1960, 01, 01), Gender.MALE);
+		Screening screening = new Screening(1000,patient,LocalDate.of(2021,01,01),false);
+		Screening screening2 = new Screening(1000,patient2,LocalDate.of(2021,01,01),true);
+		Screening screening3 = new Screening(1000,patient3,LocalDate.of(2021,01,01),false);
+		List<Screening> screeningList = new ArrayList<>();
+		screeningList.add(screening);
+		screeningList.add(screening2);
+		screeningList.add(screening3);
+		for(Screening screeningx: screeningList){
+		System.out.println("Patient: " + screeningx.getPatient().getName() + " has a malignant diagnosis of " + screeningx.isMalignant());}
+	}
+
+	private static void printService(){
+		ScreeningDatabase screeningDatabase = new ScreeningDatabase();
+		ScreeningService screeningService = new ScreeningService(screeningDatabase);
+		List<Screening> screeningList = screeningService.getScreenings();
+		for(Screening screeningx: screeningList){
+			System.out.println("Patient: " + screeningx.getPatient().getName() + " has a malignant diagnosis of " + screeningx.isMalignant());}
+	}
+
+	private static void printFindScreening(){
+		ScreeningDatabase screeningDatabase = new ScreeningDatabase();
+		ScreeningService screeningService = new ScreeningService(screeningDatabase);
+		Screening screening = screeningService.findScreening("Alan");
+		if(screening!=null){
+			System.out.println("Patient: " + screening.getPatient().getName() + " has a malignant diagnosis of " + screening.isMalignant());
+		} else{
+			System.out.println("No Screening found");
+		}
 	}
 
 }
--- src/main/java/com/accenture/tagtrainingspring/screening/ScreeningService.java
+++ src/main/java/com/accenture/tagtrainingspring/screening/ScreeningService.java
@@ -1,6 +1,7 @@
 package com.accenture.tagtrainingspring.screening;
 
 import com.accenture.tagtrainingspring.patient.Patient;
+import javafx.stage.Screen;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,23 @@ public class ScreeningService {
     @Autowired
     private ScreeningDatabase screeningDatabase;
 
+    public ScreeningService(ScreeningDatabase screeningDatabase) {
+        this.screeningDatabase = screeningDatabase;
+    }
+
     public boolean isPatientScreening(Screening screening, Patient patient) {
         return screening.getPatient().getId().equals(patient.getId());
+    }
+
+    public Screening findScreening(String name) {
+        Screening screening = null;
+        List<Screening>  list = getScreenings();
+        for(Screening screeningx: list){
+            if(screeningx.getPatient().getName().equalsIgnoreCase(name)){
+                screening = screeningx;
+            }
+        }
+        return screening;
     }
 
     public List<Screening> getScreenings() {
